7313002cadaa19b080d232bb212d206ebd24ce93
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-01-18 22:10:08 -0600

    Added action that saved task list is loaded when the page is loaded

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,5 @@ import App from './App.vue';
 import store from './store';
 
 createApp({render: () => h(App)}).use(store).mount('#app');
-store.dispatch("loadUser");//load user data
+store.dispatch('loadUser');//load user data
+store.dispatch('loadTodos');//load task list data
